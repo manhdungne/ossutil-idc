428d035da2cc14cba6733aa12970fdcd1a684a50
Hà Dũng <hamanhdung@MacBook-Pro-cua-Ha.local>
2025-10-28 16:12:01 +0700

    Fix bug same bucket

--- lib/cp.go
+++ lib/cp.go
@@ -2988,12 +2988,14 @@ func (cc *CopyCommand) checkCopyFileArgs(srcURL, destURL CloudURL) error {
 	if srcURL.bucket != destURL.bucket {
 		return nil
 	}
+
+	s3 := true
 	srcPrefix := srcURL.object
 	destPrefix := destURL.object
 	
-	if srcPrefix == destPrefix{
+	if srcPrefix == destPrefix & !s3 {
 		if cc.cpOption.meta == "" {
-			return fmt.Errorf("\"%s\" and \"%s\" are [MARK-D] the same, copy self will do nothing, set meta please use --meta options ", srcURL.ToString(), destURL.ToString())
+			return fmt.Errorf("\"%s\" and \"%s\" are [MARK-D] the same, copy self will do nothing, set meta please use --meta options ", srcURL.ToString(), destURL)
 		}
 	} else if cc.cpOption.recursive {
 		if strings.HasPrefix(destPrefix, srcPrefix) {
