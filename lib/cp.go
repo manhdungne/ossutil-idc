be7daf4badc08ac6178117e88f75dd9872ddceb7
Hà Dũng <hamanhdung@MacBook-Pro-cua-Ha.local>
2025-11-07 16:03:58 +0700

    update object log

--- lib/cp.go
+++ lib/cp.go
@@ -1706,7 +1706,7 @@ func (cc *CopyCommand) progressBar() {
             if !ok {
                 return
             }
-            fmt.Printf(cc.monitor.progressBar(signal.finish, signal.exitStat))
+            io.WriteString(os.Stderr, cc.monitor.progressBar(signal.finish, signal.exitStat))
             idle = 0
         case <-time.After(500 * time.Millisecond):
             // nếu đã yêu cầu đóng và không còn dữ liệu mới → thoát
